a48c9429c6a49db6c68d30fd9407036b505297fc
cusj3102 <65480663+JeremyCusson@users.noreply.github.com>
2020-11-24 13:11:31 -0500

    Update LED_BUZZER.cpp

--- LED_BUZZER.cpp
+++ LED_BUZZER.cpp
@@ -1,10 +1,6 @@
 #include <Arduino.h>
 #include <librobus.h>
 
-
-
-
-
 #define LED_BLUE 2 
 #define LED_WHITE 3 
 #define LED_RED 4
@@ -35,65 +31,7 @@ void setup()
 
 void loop()
 {
-    if ( out >= 0.08) // le 0.008 représente la taux d'alcool dans le sang interdit(80mg/100ml)
-    {
-        digitalWrite(LED_BLUE,HIGH);
-        delay(500);
-        digitalWrite(LED_BLUE,LOW);
-        digitalWrite(LED_WHITE,HIGH);
-        delay(500);
-        digitalWrite(LED_WHITE,LOW);
-        digitalWrite(LED_RED,HIGH);
-        delay(500);
-        digitalWrite(LED_RED,LOW);
-        digitalWrite(LED_BLUE,HIGH);
-        delay(500);
-        digitalWrite(LED_BLUE,LOW);
-        digitalWrite(LED_WHITE,HIGH);
-        delay(500);
-        digitalWrite(LED_WHITE,LOW);
-        digitalWrite(LED_RED,HIGH);
-        delay(500);
-        digitalWrite(LED_RED,LOW);
-        digitalWrite(LED_BLUE,HIGH);
-        delay(500);
-        digitalWrite(LED_BLUE,LOW);
-        digitalWrite(LED_WHITE,HIGH);
-        delay(500);
-        digitalWrite(LED_WHITE,LOW);
-        digitalWrite(LED_RED,HIGH);
-        delay(500);
-        digitalWrite(LED_RED,LOW);
-        delay(500);
-
-        digitalWrite(buzzer,300); /* 300 etant la valeur de hz du buzzer */
-        delay(10);
-        digitalWrite(buzzer,800);
-        delay(10);
-    }
-    else /* LA VALEUR BOUL EST 0 (DONC AUCUN ALCOOL DANS LE SANG)*/
-    {
-        digitalWrite(LED_GREEN1,HIGH);
-        delay(500);
-        digitalWrite(LED_GREEN2,HIGH);
-        delay(500);
-        digitalWrite(LED_GREEN3,HIGH);
-        delay(500);
-        digitalWrite(LED_GREEN1,LOW);
-        delay(500);
-        digitalWrite(LED_GREEN2,LOW);
-        delay(500);
-        digitalWrite(LED_GREEN3,LOW);
-        delay(500);
     
-    
-        for (int thisNote = 0; thisNote < 8; thisNote++) 
-        {
-            // pin8 output the voice, every scale is 0.5 sencond
-            tone(8, melody[thisNote], duration);
-            delay(1000);
-        }
-    }  
 }
 
 void LumierePolice()
@@ -116,4 +54,27 @@ void LumierePolice()
         digitalWrite(buzzer,800);
         delay(10);
         digitalWrite(buzzer, 0);
+}
+
+void LumiereVert()
+{
+    digitalWrite(LED_GREEN1,HIGH);
+    delay(500);
+    digitalWrite(LED_GREEN2,HIGH);
+    delay(500);
+    digitalWrite(LED_GREEN3,HIGH);
+    delay(500);
+    digitalWrite(LED_GREEN1,LOW);
+    delay(500);
+    digitalWrite(LED_GREEN2,LOW);
+    delay(500);
+    digitalWrite(LED_GREEN3,LOW);
+    delay(500);
+    
+    for (int thisNote = 0; thisNote < 8; thisNote++) 
+    {
+        // pin8 output the voice, every scale is 0.5 sencond
+        tone(8, melody[thisNote], duration);
+        delay(1000);
+    }
 }
